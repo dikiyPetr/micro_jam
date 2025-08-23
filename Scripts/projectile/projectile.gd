7dc419bffeccf56bb114274338a074303b54601b
Bibuba <princesakenny98@gmail.com>
2025-08-23 20:39:13 +0300

    add penetration

--- Scripts/projectile/projectile.gd
+++ Scripts/projectile/projectile.gd
@@ -2,27 +2,28 @@ extends Area2D
 class_name Projectile
 
 @onready var damagable: Damagable = $Damagable
-var _speed: float = 1
-var _lifetime: float = 1
+var _stat: WeaponStat
 var _dir: Vector2 = Vector2.RIGHT
 var _life: float
-
+var _penetrateCount = 0
 func _ready() -> void:
-	_life = _lifetime
+	_life = 1
 
 func setup(direction: Vector2, stat: WeaponStat) -> void:
 	_dir = direction.normalized()
-	_speed = stat.bulletSpeed
-	_lifetime = stat.bulletLifetime
+	_stat = stat
+	_life = stat.bulletLifetime
 	damagable.damage_amount = stat.damage
 
 func _physics_process(delta: float) -> void:
-	global_position += _dir * _speed * delta
+	global_position += _dir * _stat.bulletSpeed * delta
 	_life -= delta
 	if _life <= 0.0:
 		queue_free()
 
 func onHit(damage: float) -> void:
+	if damage > 0:
+		_penetrateCount = _penetrateCount + 1
+		if _penetrateCount >= _stat.penetration:
+			queue_free()
 	pass
-	#if damage > 0:
-		#queue_free()
--- Scripts/stats/weapon_stat.gd
+++ Scripts/stats/weapon_stat.gd
@@ -1,6 +1,6 @@
 class_name WeaponStat
 var damage := 1
-var penetration := 1
+var penetration := 2
 var range: float = 560.0
 var fire_rate: float = 3.0
 var bullets_per_shot: int = 1
